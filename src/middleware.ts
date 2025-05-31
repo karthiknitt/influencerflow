d70a8d41bff78c19c603f28b069f11c97367fe17
Karthikeyan Natarajan <karthiknitt@gmail.com>
2025-05-31 16:09:53 +0530

    clerk middleware bug fixed.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,16 @@
-import { withClerkMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default withClerkMiddleware(
-  (req, res, next) => {
-    return next();
-  },
-  {
-    publicRoutes: ["/", "/sign-in(.*)", "/sign-up(.*)"],
-    ignoredRoutes: ["/api/webhooks(.*)"],
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
+
+export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) {
+    auth.protect();
   }
-);
+
+  return NextResponse.next();
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ['/((?!.*\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
